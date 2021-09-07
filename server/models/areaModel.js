0ed477a1c7ab94796bfd3b186a857f70b553bde6
sean kim <s3jkim@gmail.com>
2021-09-07 15:57:22 -0700

    starting the area model tree structure

---
+++ server/models/areaModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AreaSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    boulders: [{ type: Schema.Types.ObjectId, ref: "Boulder" }],
+    path: [{ type: Schema.Types.ObjectId, ref: "Area" }],
+    parent: {
+        type: Schema.Types.ObjectId, ref: "Area"
+    }
+})
+
+module.exports = mongoose.model("Area", AreaSchema);
